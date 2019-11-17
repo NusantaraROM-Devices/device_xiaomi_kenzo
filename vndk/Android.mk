efeed679ba0b8d290cfc71ff988f7949497a4a2e
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-18 00:58:14 +0200

    Revert "msm8956: vndk: Update from marlin"
    This reverts commit c156f99503dd2f337e59b071b3ecea4f4592a67d.

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,23 +1,6 @@
-# TODO(b/69526027): DEPRECATE USE OF THIS.
-# USE BOARD_VNDK_VERSION:=current instead.
-
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/vndk-sp-libs.mk
-
-ifndef BOARD_VNDK_VERSION
-# The libs with "vndk: {enabled: true, support_system_process: true}" will be
-# added VNDK_SP_LIBRARIES automatically. And the core variants of the VNDK-SP
-# libs will be copied to vndk-sp directory.
-# However, some of those libs need FWK-ONLY libs, which must be listed here
-# manually.
-VNDK_SP_LIBRARIES := \
-    libdexfile_support
-
-install_in_hw_dir := \
-   android.hidl.memory@1.0-impl
-
-vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
 define define-vndk-sp-lib
 include $$(CLEAR_VARS)
@@ -29,8 +12,7 @@ LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
-LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE_RELATIVE_PATH := vndk-sp
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -44,8 +26,7 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
-LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE_RELATIVE_PATH := vndk-sp
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
@@ -54,12 +35,8 @@ endef
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
     $(eval $(call define-vndk-sp-lib,$(lib))))
 
-install_in_hw_dir :=
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
-LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
-endif
