48fc2fa0a2e78d3c7e41a17fc8c35be7104802ea
Magicxavi <rolfpeterkesche@googlemail.com>
2019-12-09 17:42:49 +0200

    msm8956: Set power targets by default

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,10 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
+# Power
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_NON_LEGACY_POWERHAL := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 
