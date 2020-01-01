aranhid/device_xiaomi_msm8956-common

msm8956-common: Fix mobile network
Change-Id: I7348a9c581f9ec2259a4f0b6429a195d61734a78

msm8956.mk
@@ -247,7 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbase_shim \
     ims-ext-common \
-    ims-ext-common_system
+    ims-ext-common_system \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system

NusantaraROM-Devices/device_xiaomi_kenzo

msm8956.mk
@@ -245,7 +245,10 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common
+    ims-ext-common \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
 
 # IPA Manager
 #PRODUCT_PACKAGES += \