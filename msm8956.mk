54a9e63f6cc04bb4c7b10e847786c7762d40610a
Magicxavi <rolfpeterkesche@gmail.com>
2020-04-14 00:39:49 +0300

    msm8956-common: Add local override package to remove standart apps again
    ooh i forgot to add to packages :/
    o shiet, forgot to clean the cherry-pick left

--- msm8956.mk
+++ msm8956.mk
@@ -155,6 +155,7 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     CameraGo \
     GalleryGo \
+    RemovePackages \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
