03202035af6a4f22653239a104d274ee1c359ab4
mocarafee <harif.rp05@gmail.com>
2020-04-24 22:56:47 +0700

    prebuilt: add Yuzu Browser

--- msm8956.mk
+++ msm8956.mk
@@ -155,6 +155,7 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     CameraGo \
     GalleryGo \
+    Yuzu \
     RemovePackages \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
