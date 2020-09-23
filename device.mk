822c4a446215eaf1e46524abd6a21084d2811fa9
Magicxavi <rolfpeterkesche@gmail.com>
2020-09-23 20:04:17 +0300

    kenzo: its not a chargeonlymode anymore

--- device.mk
+++ device.mk
@@ -294,7 +294,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service \
     android.hardware.health@2.0 \
-    chargeonlymode
+    charger
 
 # HW crypto
 PRODUCT_PACKAGES += \
