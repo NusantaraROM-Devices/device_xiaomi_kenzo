82ffe8d9e20486d39b2905b0535a8ce1c788c3fb
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-04 07:37:20 +0200

    kenzo: kang bluetooth blobs from daisy
    * from xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.11.0.QDLMIXM:user/release-keys

--- config.fs
+++ config.fs
@@ -31,6 +31,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
 [system/bin/cnss-daemon]
 mode: 0755
 user: AID_BLUETOOTH
@@ -99,7 +111,7 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_RADIO
+user: AID_SYSTEM
 group: AID_RADIO
 caps: WAKE_ALARM
 
