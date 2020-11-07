80454f35168f0d6f055d3ce11c6c1de317085660
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-07 11:40:39 +0200

    kenzo: kang some ims blobs from daisy
    * Also update configurations
    * from xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.11.0.QDLMIXM:user/release-keys

--- device.mk
+++ device.mk
@@ -309,13 +309,12 @@ PRODUCT_PACKAGES += \
     libbase_shim \
     ims-ext-common \
     ims_ext_common.xml \
-    ims-ext-common_system \
     android.hardware.radio@1.0 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
+    ims-ext-common
 
 # IPA Manager
 #PRODUCT_PACKAGES += \
