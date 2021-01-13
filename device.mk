8d977cb091683512c2a8260cf3c186e42fc06407
Wang Han <416810799@qq.com>
2021-01-14 01:07:51 +0200

    kenzo: Switch to TimeKeep
     * SE policies are imported and modified from
       https://github.com/sonyxperiadev/device-sony-sepolicy.
       Modifications are needed because qcom legacy policy
       contains conflicting labels and rules.
    
    Change-Id: Id04a824dea69976f6fc9d48bef77859cc82971ed

--- device.mk
+++ device.mk
@@ -489,6 +489,11 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8952
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
